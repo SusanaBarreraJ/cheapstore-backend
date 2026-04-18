2747b2fabb1e1e3b876c859ab8efce62afa6d0c8
Susana Barrera Jara <susanabarrerajara@gmail.com>
2026-04-18 23:18:49 +0000

    limpieza de archivos

--- backend/src/main/java/com/cheapstore/backend/controller/VerificationTokenController.java
+++ backend/src/main/java/com/cheapstore/backend/controller/VerificationTokenController.java
@@ -1,20 +1,9 @@
 package com.cheapstore.backend.controller;
 
-import com.cheapstore.backend.model.Usuario;
-import com.cheapstore.backend.model.VerificationToken;
-import com.cheapstore.backend.repository.UsuarioRepository;
-import com.cheapstore.backend.service.VerificationTokenService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/token")
 public class VerificationTokenController {
-
-
 
 }
--- backend/src/main/java/com/cheapstore/backend/service/AuthService.java
+++ backend/src/main/java/com/cheapstore/backend/service/AuthService.java
@@ -19,8 +19,6 @@ import org.springframework.stereotype.Service;
 import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Optional;
-import java.util.UUID;
 
 @Service
 public class AuthService {
